36dd8fcdbd1ba394ea0343c4ec46ab170d79742b
dnuccio80 <nuccio227@gmail.com>
2024-08-29 13:15:38 -0300

    Navmesh Player modified

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,18 +7,32 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     private NavMeshAgent playerNavMeshAgent;
+    private Animator animator;
+
 
     private void Awake()
     {
         playerNavMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             MoveToCursor();
         }
+
+        HandleAnimationsVariables();
+    }
+
+    private void HandleAnimationsVariables()
+    {
+        Vector3 velocity = playerNavMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        animator.SetFloat("ForwardSpeed", localVelocity.z);
+
     }
 
     private void MoveToCursor()
